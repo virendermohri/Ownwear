3fc0aeac433a7ac7063e506e722bd512643fb5ac
virendermohri <virendermohri@gamil.com>
2024-01-26 14:03:00 +0530

    Project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='text-7xl bg-blue-400'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
